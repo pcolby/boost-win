c569deda756c8e844b282e8ddcce45316a760b3f
Paul Colby <git@colby.id.au>
2013-02-10 05:30:52 +0000

    Refactored the code-extraction / bootstrapping.

--- build.cmd
+++ build.cmd
@@ -22,34 +22,46 @@ if "%3" == "" ( set TARGET_OS=/xp ) else set TARGET_OS=%3
 call "%SET_ENV%" %MODE% %TARGET_ARCH% %TARGET_OS%
 goto :EOF
 
-:configureBoost
-@echo ==== Configuring Boost ====
+:: usage: call:extractsource build_dir
+:extractSource
 set SOURCE_DIR=%~dp0source
 set SOURCE_FILE=%SOURCE_DIR%\boost_%BOOST_VERSION%.zip
 if exist "%SOURCE_DIR%\boost_%BOOST_VERSION%.7z" (
   set SOURCE_FILE=%SOURCE_DIR%\boost_%BOOST_VERSION%.7z
 )
-set BUILD_DIR=%~dp0build\boost_%BOOST_VERSION%-%1
-if not exist "%BUILD_DIR%" (
-  md "%BUILD_DIR%"
-  @echo Extracting "%SOURCE_FILE%" to "%BUILD_DIR%"
-  "%ZIP7%" x -o"%BUILD_DIR%" "%SOURCE_FILE%" > nul
+if not exist "%~1" (
+  @echo ==== Extracting Boost Source ====
+  @echo Extracting "%SOURCE_FILE%" to "%~1"
+  "%ZIP7%" x -o"%~1" "%SOURCE_FILE%" > nul
   if errorlevel 1 (
     echo Failed to extract "%SOURCE_FILE%"
 	pause
-	rmdir "%BUILD_DIR%"
+	rmdir "%~1"
 	exit errorlevel
   )
 )
-pushd "%BUILD_DIR%\boost_%BOOST_VERSION%"
+goto :EOF
+
+:: usage: call:bootstrap boost_dir
+:bootstrap
+@echo ==== Bootstrapping Boost %~1 ====
+pushd "%~1"
 call bootstrap.bat
 popd
 goto :EOF
 
+:: usage: call:build x86|x64|ia64 debug|release
+:build
+call:configureWinSDK %~1 %~2
+set BUILD_DIR=%~dp0build\boost_%BOOST_VERSION%-%~1
+if not exist "%BUILD_DIR%" call:extractSource %BUILD_DIR%
+set BOOST_DIR=%BUILD_DIR%\boost_%BOOST_VERSION%
+if not exist "%BOOST_DIR%\b2.exe" call:bootstrap %BOOST_DIR%
+:: @todo
+goto :EOF
+
 :main
 if not exist "%~dp0build" md "%~dp0build"
-call:configureWinSDK x86 release
-call:configureBoost x86
-call:configureWinSDK x64 release
-call:configureBoost x64
+call:build x86 release
+call:build x64 release
 pause
